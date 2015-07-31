62a8692c71d2e3adf5ca88137049dfd63d81c2e9
dmf444 <becat444@gmail.com>
2015-07-30 23:15:14 -0400

    Found the problem:
    -Fix IML, gets reset by VirtualFiles
    -Reload Project as .ipr

--- src/IntelliForge/NewForgeMod/NewForgeModBuilder.java
+++ src/IntelliForge/NewForgeMod/NewForgeModBuilder.java
@@ -14,8 +14,10 @@ import com.intellij.openapi.Disposable;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.module.ModuleType;
 import com.intellij.openapi.options.ConfigurationException;
+import com.intellij.openapi.roots.ContentEntry;
 import com.intellij.openapi.roots.ModifiableRootModel;
 import com.intellij.openapi.vfs.VirtualFile;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.io.*;
@@ -26,24 +28,68 @@ import java.nio.file.StandardCopyOption;
 /**
  * Created by David on 27/07/2015.
  */
-public class NewForgeModBuilder extends ModuleBuilder implements ModuleBuilderListener{
+public class NewForgeModBuilder extends ModuleBuilder {//implements ModuleBuilderListener
 
     private String Version;
     private String MC;
 
     public NewForgeModBuilder(){
-        addListener(this);
+       // addListener(this);
     }
 
 
     @Override
     public void setupRootModel(ModifiableRootModel modifiableRootModel) throws ConfigurationException {
-
-        System.out.println(modifiableRootModel.getContentEntries());
+        final ContentEntry contentreg = doAddContentEntry(modifiableRootModel);
+        final VirtualFile baseDir = contentreg == null ? null : contentreg.getFile();
+        if(baseDir != null){
+            setupProject(modifiableRootModel, baseDir);
+        }
 
        // System.out.println("setupRootModel");
        // modifiableRootModel.getProject().getBaseDir().getCanonicalPath();
 
+    }
+
+    public void setupProject(@NotNull final ModifiableRootModel rootModel, @NotNull final VirtualFile basedir){
+        if(basedir.exists()){
+            ParseCollection p = new ParseCollection(new ParseCollection.VersionPolicy() {
+                @Override
+                public boolean downloadMcVersion(String version) {
+                    if(version.startsWith("1.6.4")){
+                        return true;
+                    }else if(version.startsWith("1.7")){
+                        return true;
+                    }else if(version.startsWith("1.8")){
+                        return true;
+                    }else {
+                        return false;
+                    }
+                }
+            });
+            BuildData BD = p.getVersion(Version);
+            String downloadLink = BD.downloadLink;
+            try {
+                URL website = new URL("http://" + downloadLink);
+                Files.copy(website.openStream(), new File(basedir.getCanonicalPath(),  "forge.zip").toPath(), StandardCopyOption.REPLACE_EXISTING);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            try {
+                new File(basedir.getCanonicalPath() + "/" + new File(basedir.getCanonicalPath()).getName()).mkdir();
+                Unzip.unzip(basedir.getCanonicalPath() + "/forge.zip",basedir.getCanonicalPath() + "/" + new File(basedir.getCanonicalPath()).getName());
+
+            } catch (IOException e) {e.printStackTrace();}
+            //System.out.println("I think i finished!");
+
+
+            MultipleExecuteCommandThread a = new MultipleExecuteCommandThread(
+                    new Thing(basedir, basedir.getCanonicalPath(), basedir.getCanonicalPath() + "/" + new File(basedir.getCanonicalPath()).getName()),
+                    new ExecuteCommandThread(OperatingSystemHelper.systemHelper.getOSexecuteString(), "setupDecompWorkspace", basedir.getCanonicalPath() + "/" + new File(basedir.getCanonicalPath()).getName(), OperatingSystemHelper.systemHelper.isWindows()),
+                    new ExecuteCommandThread(OperatingSystemHelper.systemHelper.getOSexecuteString(), "idea", basedir.getCanonicalPath() + "/" + new File(basedir.getCanonicalPath()).getName(), OperatingSystemHelper.systemHelper.isWindows()));
+            a.start();
+        }
     }
 
     public void setVersion(String s){
@@ -65,7 +111,7 @@ public class NewForgeModBuilder extends ModuleBuilder implements ModuleBuilderLi
         return new NewForgeModSteps(this);
     }
 
-    @Override
+    /*@Override
     public void moduleCreated(Module module) {
 
         this.deleteModuleFile(module.getModuleFilePath());
@@ -105,9 +151,10 @@ public class NewForgeModBuilder extends ModuleBuilder implements ModuleBuilderLi
         //System.out.println("I think i finished!");
 
 
-        MultipleExecuteCommandThread a = new MultipleExecuteCommandThread(new Thing(module.getModuleFile(), module.getProject().getBaseDir().getCanonicalPath(), module.getProject().getBaseDir().getCanonicalPath() + "/" + new File(module.getProject().getBaseDir().getCanonicalPath()).getName()), new ExecuteCommandThread(OperatingSystemHelper.systemHelper.getOSexecuteString(), "setupDecompWorkspace", module.getProject().getBaseDir().getCanonicalPath() + "/" + new File(module.getProject().getBaseDir().getCanonicalPath()).getName(), OperatingSystemHelper.systemHelper.isWindows()),
-
-        new ExecuteCommandThread(OperatingSystemHelper.systemHelper.getOSexecuteString(), "idea", module.getProject().getBaseDir().getCanonicalPath() + "/" + new File(module.getProject().getBaseDir().getCanonicalPath()).getName(), OperatingSystemHelper.systemHelper.isWindows()));
+        MultipleExecuteCommandThread a = new MultipleExecuteCommandThread(
+                new Thing(module.getModuleFile(), module.getProject().getBaseDir().getCanonicalPath(), module.getProject().getBaseDir().getCanonicalPath() + "/" + new File(module.getProject().getBaseDir().getCanonicalPath()).getName()),
+                new ExecuteCommandThread(OperatingSystemHelper.systemHelper.getOSexecuteString(), "setupDecompWorkspace", module.getProject().getBaseDir().getCanonicalPath() + "/" + new File(module.getProject().getBaseDir().getCanonicalPath()).getName(), OperatingSystemHelper.systemHelper.isWindows()),
+                new ExecuteCommandThread(OperatingSystemHelper.systemHelper.getOSexecuteString(), "idea", module.getProject().getBaseDir().getCanonicalPath() + "/" + new File(module.getProject().getBaseDir().getCanonicalPath()).getName(), OperatingSystemHelper.systemHelper.isWindows()));
         a.start();
 
 
@@ -123,20 +170,10 @@ public class NewForgeModBuilder extends ModuleBuilder implements ModuleBuilderLi
 
         } catch (IOException e) {
             e.printStackTrace();
-        }*/
+        }
 
-    }
+    }*/
 
-    private static void executeCMD(String osCMD, String cmd, String FileLoc, boolean windows){
-        try {
-            Process p;
-            if(!windows){
-                Runtime.getRuntime().exec("chmod +x ./gradlew", null, new File(FileLoc));
-            }
-            p =  Runtime.getRuntime().exec(osCMD + " " + cmd, null, new File(FileLoc));
-            p.waitFor();
-        } catch (IOException | InterruptedException e) {}
-    }
 
     private static class Thing implements Runnable {
 
@@ -175,7 +212,7 @@ public class NewForgeModBuilder extends ModuleBuilder implements ModuleBuilderLi
         File f;
 
         public Thing(VirtualFile f, String t, String fff){
-
+            removeAll(f);
             ff =f ;
 
             this.t = new File(t);
@@ -184,6 +221,15 @@ public class NewForgeModBuilder extends ModuleBuilder implements ModuleBuilderLi
 
         }
 
+        private void removeAll(VirtualFile base){
+            VirtualFile[] files = base.getChildren();
+            for(int i = 0; i < files.length; i++){
+                VirtualFile file = files[i];
+                if(file.getExtension() != null && file.getExtension().equals(".iml")){
+                    try {   file.delete("Carl");    } catch (IOException e) {}
+                }
+            }
+        }
 
         @Override
         public void run() {
